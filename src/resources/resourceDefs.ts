3ef7cfeb8d502536ecd1b79e68e2b101777fdc72
Jaclyn Brockschmidt <jcbrockschmidt@gmail.com>
2022-07-27 23:57:58 -0700

    Add more resources

--- src/resources/resourceDefs.ts
+++ src/resources/resourceDefs.ts
@@ -1,32 +1,64 @@
 import { IResourceBrick } from './types';
 
-// TODO: material types, make non-filler resources glow or something
-
 export const DirtResource: IResourceBrick = {
     name: 'dirt',
     displayName: 'Dirt',
     color: [51, 45, 26],
-    // hp: 5,
-    hp: 1, // DEBUG
+    hp: 5,
 };
 
 export const StoneResource: IResourceBrick = {
     name: 'stone',
     displayName: 'Stone',
     color: [145, 142, 133],
-    hp: 5,
+    hp: 10,
 };
 
-export const IronResource: IResourceBrick = {
-    name: 'iron',
-    displayName: 'Iron',
-    color: [165, 156, 148],
-    hp: 5,
+export const QuartzResource: IResourceBrick = {
+    name: 'quartz',
+    displayName: 'Quartz',
+    color: [233, 223, 224],
+    hp: 10,
+};
+
+export const AluminiumResource: IResourceBrick = {
+    name: 'aluminium',
+    displayName: 'Aluminium',
+    color: [136, 139, 141],
+    hp: 15,
 };
 
 export const CopperResource: IResourceBrick = {
     name: 'copper',
     displayName: 'Copper',
     color: [184, 115, 51],
-    hp: 5,
+    hp: 25,
+};
+
+export const IronResource: IResourceBrick = {
+    name: 'iron',
+    displayName: 'Iron',
+    color: [165, 156, 148],
+    hp: 30,
+};
+
+export const SilverResource: IResourceBrick = {
+    name: 'silver',
+    displayName: 'Silver',
+    color: [211, 211, 211],
+    hp: 50,
+};
+
+export const GoldResource: IResourceBrick = {
+    name: 'gold',
+    displayName: 'Gold',
+    color: [255, 215, 0],
+    hp: 100,
+};
+
+export const SapphireResource: IResourceBrick = {
+    name: 'sapphire',
+    displayName: 'Sapphire',
+    color: [15, 82, 186],
+    hp: 200,
 };
