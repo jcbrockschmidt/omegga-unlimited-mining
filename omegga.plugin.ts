2b02d5c7714218bba3b6af5aa2d01bef30808bed
Jaclyn Brockschmidt <jcbrockschmidt@gmail.com>
2022-09-14 00:45:25 -0700

    Add /inventory alongside /inv

--- omegga.plugin.ts
+++ omegga.plugin.ts
@@ -1,4 +1,4 @@
-import { OL, PS, PC, Vector } from 'omegga';
+import { OL, PS, PC, Vector, OmeggaPlayer } from 'omegga';
 import {
   IMine,
   Mine,
@@ -26,71 +26,87 @@ export default class Plugin implements UMPlugin {
     this.mine = new Mine(this, MINE_ORIGIN, MINE_WIDTH, MINE_HEIGHT);
   }
 
+  async onLeave({ id }: OmeggaPlayer) {
+    try {
+      await PlayerDataManager.savePlayerData(this, id);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async onCmdCreateMine(playerName: string) {
+    try {
+      if (this.mine.isCreated()) {
+        this.omegga.whisper(playerName, 'Mine already created');
+        return;
+      }
+      this.omegga.broadcast('Creating mine...');
+      await this.mine.createMine();
+      this.omegga.broadcast('Mine created');
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async onCmdClearMine(playerName: string) {
+    try {
+      if (!this.mine.isCreated()) {
+        this.omegga.whisper(playerName, 'No mine to clear');
+        return;
+      }
+      this.omegga.broadcast('Clearing mine...');
+      await this.mine.clearMine();
+      this.omegga.broadcast('Mine cleared');
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async onCmdResetMine(playerName: string) {
+    try {
+      if (!this.mine.isCreated()) {
+        this.omegga.whisper(playerName, 'No mine to reset');
+        return;
+      }
+      this.omegga.broadcast('Resetting mine...');
+      await this.mine.clearMine();
+      await this.mine.createMine();
+      this.omegga.broadcast('Mine reset');
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async onCmdInventory(playerName: string) {
+    try {
+      const player = this.omegga.getPlayer(playerName);
+      const playerData = await PlayerDataManager.getPlayerData(this, player.id);
+      playerData.displayInventory();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   async init() {
     // TODO: delete world plate
 
     this.omegga
-      .on('leave', async ({ id }) => {
-        try {
-          await PlayerDataManager.savePlayerData(this, id);
-        } catch (e) {
-          console.log(e);
-        }
-      })
-      .on('cmd:createmine', async (playerName: string) => {
-        try {
-          if (this.mine.isCreated()) {
-            this.omegga.whisper(playerName, 'Mine already created');
-            return;
-          }
-          this.omegga.broadcast('Creating mine...');
-          await this.mine.createMine();
-          this.omegga.broadcast('Mine created');
-        } catch (e) {
-          console.log(e);
-        }
-      })
-      .on('cmd:clearmine', async (playerName: string) => {
-        try {
-          if (!this.mine.isCreated()) {
-            this.omegga.whisper(playerName, 'No mine to clear');
-            return;
-          }
-          this.omegga.broadcast('Clearing mine...');
-          await this.mine.clearMine();
-          this.omegga.broadcast('Mine cleared');
-        } catch (e) {
-          console.log(e);
-        }
-      })
-      .on('cmd:resetmine', async (playerName: string) => {
-        try {
-          if (!this.mine.isCreated()) {
-            this.omegga.whisper(playerName, 'No mine to reset');
-            return;
-          }
-          this.omegga.broadcast('Resetting mine...');
-          await this.mine.clearMine();
-          await this.mine.createMine();
-          this.omegga.broadcast('Mine reset');
-        } catch (e) {
-          console.log(e);
-        }
-      })
-      .on('cmd:inv', async (playerName: string) => {
-        try {
-          const player = this.omegga.getPlayer(playerName);
-          const playerData = await PlayerDataManager.getPlayerData(
-            this,
-            player.id
-          );
-          playerData.displayInventory();
-        } catch (e) {
-          console.log(e);
-        }
-      });
+      .on('leave', this.onLeave.bind(this))
+      .on('cmd:createmine', this.onCmdCreateMine.bind(this))
+      .on('cmd:clearmine', this.onCmdClearMine.bind(this))
+      .on('cmd:resetmine', this.onCmdResetMine.bind(this))
+      .on('cmd:inventory', this.onCmdInventory.bind(this))
+      .on('cmd:inv', this.onCmdInventory.bind(this));
 
-    return { registeredCommands: ['createmine', 'clearmine', 'resetmine'] };
+    return {
+      registeredCommands: [
+        'createmine',
+        'clearmine',
+        'resetmine',
+        'inventory',
+        'inv',
+      ],
+    };
   }
 
   async stop() {
@@ -99,7 +115,9 @@ export default class Plugin implements UMPlugin {
     this.omegga
       .removeAllListeners('cmd:createmine')
       .removeAllListeners('cmd:clearmine')
-      .removeAllListeners('cmd:resetmine');
+      .removeAllListeners('cmd:resetmine')
+      .removeAllListeners('cmd:inventory')
+      .removeAllListeners('cmd:inv');
 
     if (this.mine.isCreated()) {
       await this.mine.clearMine();
